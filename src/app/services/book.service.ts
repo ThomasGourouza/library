b2289557abe1af84349f177117de8891bf4b8d68
Thomas Gourouza <thomas.gourouza@de.markant.com>
2025-07-03 09:59:58 +0200

    refacto bookservice create id

--- src/app/services/book.service.ts
+++ src/app/services/book.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { Book } from '@shared/constants';
 import { UtilsService } from './utils.service';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +14,18 @@ export class BookService {
 
   get books$(): Observable<Book[]> {
     return this.http.get<Book[]>(this.url).pipe(
-      map((books) => books ?? []),
-      catchError(() => of([] as Book[]))
+      map(this.withTitleAndId.bind(this)),
+      catchError(() => of([] as Book[])),
+      shareReplay(1)
     );
+  }
+
+  private withTitleAndId(books: Book[]): Book[] {
+    return books
+      .filter(({ title }) => !!title)
+      .map((book) => ({
+        ...book,
+        id: this.utilsService.makeId(book['title']!),
+      }));
   }
 }
